b6db54f35488a44fe7c83f40877c1a485ac23f21
lbenav8095 <lubensaico@gmail.com>
2024-01-14 16:12:49 -0600

    Initial commit

---
+++ src/main/java/me/anjoismysign/Skeramid.java
@@ -0,0 +1,24 @@
+package me.anjoismysign;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface Skeramid<T> {
+    /**
+     * Will get the parent of this floor.
+     * In case of being the peak, will return itself.
+     *
+     * @return The parent, or itself if it's the peak.
+     */
+    @NotNull
+    T getParent();
+
+    /**
+     * Will get the child of this floor.
+     * If it doesn't exist, it will create it.
+     *
+     * @param argument The argument to get the child.
+     * @return The child.
+     */
+    @NotNull
+    T child(String argument);
+}
